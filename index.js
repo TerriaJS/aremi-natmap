f2f8b4e95d0a19181ceded98b590dba5c5359321
Mats Henrikson <mats@zakalwe.com>
2016-01-15 12:08:38 +1100

    Open the Feedback link in a new window/tab.

--- index.js
+++ index.js
@@ -422,8 +422,8 @@ terria.start({
     document.querySelector(".explorer-panel-body-pane").appendChild(surveyContainer);
     surveyContainer.innerHTML =
       '<div class="feedback-request">\
-        <p><a href="' + surveyLink + '">Give Feedback</a></p>\
-        <p>This site is still in development, so please <a href="' + surveyLink + '">tell us what you think</a>. Our survey will take 5 mins.</p>\
+        <p><a target="_blank" href="' + surveyLink + '">Give Feedback</a></p>\
+        <p>This site is still in development, so please <a target="_blank" href="' + surveyLink + '">tell us what you think</a>. Our survey will take 5 mins.</p>\
       </div>';
 
     MapProgressBarViewModel.create({
