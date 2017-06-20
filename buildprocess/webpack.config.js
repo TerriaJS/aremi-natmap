14c4a180eb32a1f97168200371b1d506c3400baa
Ashley Stacey <ashley.stacey@data61.csiro.au>
2017-06-20 15:57:37 +1000

    Force chunk CSS into main nationalmap.css
    Commit from:
    https://github.com/TerriaJS/TerriaMap.git
    063b052062aaca3267919a8c36822cb12508ad1a
    Original Commit Author:
    Stephen Davies <steve9164@gmail.com>  2017-05-09 15:37:46
    
    Conflicts:
    buildprocess/webpack.config.js

--- buildprocess/webpack.config.js
+++ buildprocess/webpack.config.js
@@ -90,7 +90,7 @@ module.exports = function(devMode, hot) {
                     'NODE_ENV': devMode ? '"development"' : '"production"'
                 }
             }),
-            new ExtractTextPlugin({filename: "nationalmap.css", disable: hot, ignoreOrder: true})
+            new ExtractTextPlugin({filename: "nationalmap.css", disable: hot, ignoreOrder: true, allChunks: true})
         ],
        resolve: {
             alias: {},
