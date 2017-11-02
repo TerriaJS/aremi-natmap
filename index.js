1d0e3618c65f0a954483debdf513d38bb437f90e
Mats Henrikson <mats@zakalwe.com>
2017-11-02 20:31:29 +1100

    Making the basemap filtering betterer, thanks @kring! :D

--- index.js
+++ index.js
@@ -138,13 +138,9 @@ terria.start({
         var globalBaseMaps = createGlobalBaseMapOptions(terria, configuration.bingMapsKey);
         var extraBaseMaps = createExtraBaseMapOptions(terria);
 
-        var toRemove = ["Bing Maps Roads", "Natural Earth II", "NASA Black Marble"];
-        var matchBasemapName = function(basemap) {
-            return !(toRemove.includes(basemap.catalogItem.name));
-        };
-
         var allBaseMaps = australiaBaseMaps.concat(globalBaseMaps.concat(extraBaseMaps));
-        allBaseMaps = allBaseMaps.filter(matchBasemapName);
+        var toRemove = ["Bing Maps Roads", "Natural Earth II", "NASA Black Marble"];
+        allBaseMaps = allBaseMaps.filter(basemap => toRemove.indexOf(basemap.catalogItem.name) < 0);
         selectBaseMap(terria, allBaseMaps, 'Positron (Light)', true);
 
         // Add the disclaimer, if specified
